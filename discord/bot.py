1751488a65752b031318ef33f2adc5298dc4b30d
Connor Swislow <52296992+ConnorSwis@users.noreply.github.com>
2021-05-21 10:10:18 -0400

    DONT PUSH BOT TOKEN

--- discord/bot.py
+++ discord/bot.py
@@ -4,6 +4,7 @@ import os
 
 
 PREFIX = '$'
+TOKEN = 'XXX'
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=PREFIX, owner_id=640393413425889314, intents=intents)
 
@@ -14,4 +15,4 @@ for filename in os.listdir(COG_FOLDER):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('ODM5MzM4NjAyNjcyMjkxOTE0.YJIM_Q.vnhUagjiLuwVs-CtRQ758vzJ6n4')
+client.run(TOKEN)
